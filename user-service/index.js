b552627f72cb6d0baeb3b36be9d973fc50e38a5f
Ayyah-Coded <gbolahanyahyah@gmail.com>
2026-07-01 00:35:51 +0100

    - Implement the user service - Containerization the user service using Dockerfile - Implement docker-compose.yml to spin up the user service

---
+++ user-service/index.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+
+
+const app = express();
+
+app.use(bodyParser.json())
+
+const PORT = 3001
+
+mongoose.connect('mongodb://localhost:27017/users')
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.error("MongoDB connection error: ", err))
+
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: String
+});
+
+const User = mongoose.model("User", UserSchema);
+
+app.get('/users', async (req, res) => {
+  const users = await User.find();
+
+  res.json(users);
+})
+
+app.post('/users', async (req, res) => {
+  const { name, email } = req.body
+
+  try {
+    const user = new User({ name, email });
+    await user.save();
+
+    res.status(201).json(user);
+  } catch (error) {
+    console.error("Error saving: ", error);
+
+    res.status(500).json({ error: "Internal Server Error" })
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`User Service listening on PORT: ${PORT}`)
+});
+
